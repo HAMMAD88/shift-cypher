b5785b08e3e1a05b322adcf80da16bf26ca82f57
HAMMAD JAVAID <javedwahlahammad@gmail.com>
2019-12-11 21:17:58 +0500

    2 commit

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,1 +1,35 @@
-#
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main() {
+	fstream file;
+	char word;
+
+
+
+	// opening file 
+	file.open("plaintext.txt");
+
+	// extracting words from the file 
+	while (file >> word)
+	{
+		// displaying content
+		if (file.peek() == '\n') {
+			//cout << word;
+			//cout << "\n";
+			cout << "\nascii of next line:";
+			cout << int(file.peek());
+			cout << endl;
+		}
+		else if (file.peek() == ' ') {
+			//cout << word;
+			//cout << " ";
+			cout << "ascii of space:";
+			cout << int(file.peek());
+		}
+		else {
+			//cout << word;
+		}
+	}
+}
